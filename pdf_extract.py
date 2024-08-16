4a44a57a17d4b97d9dfb42e7207ccb4ad303e079
debarun-ghosh17 <105906064+debarun-ghosh17@users.noreply.github.com>
2024-08-16 15:18:42 +0530

    Update pdf_extract.py

--- pdf_extract.py
+++ pdf_extract.py
@@ -148,119 +148,13 @@ if __name__ == '__main__':
             )
             doc_layout_result.append(layout_res)
             
-        # Formula recognition, collect all formula images in whole pdf file, then batch infer them.
-        a = time.time()  
-        dataset = MathDataset(mf_image_list, transform=mfr_transform)
-        dataloader = DataLoader(dataset, batch_size=128, num_workers=32)
-        mfr_res = []
-        for imgs in dataloader:
-            imgs = imgs.to(device)
-            output = mfr_model.generate({'image': imgs})
-            mfr_res.extend(output['pred_str'])
-        for res, latex in zip(latex_filling_list, mfr_res):
-            res['latex'] = latex_rm_whitespace(latex)
-        b = time.time()
-        print("formula nums:", len(mf_image_list), "mfr time:", round(b-a, 2))
-            
-        # ocr and table recognition
-        for idx, image in enumerate(img_list):
-            pil_img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-            single_page_res = doc_layout_result[idx]['layout_dets']
-            single_page_mfdetrec_res = []
-            for res in single_page_res:
-                if int(res['category_id']) in [13, 14]:
-                    xmin, ymin = int(res['poly'][0]), int(res['poly'][1])
-                    xmax, ymax = int(res['poly'][4]), int(res['poly'][5])
-                    single_page_mfdetrec_res.append({
-                        "bbox": [xmin, ymin, xmax, ymax],
-                    })
-            for res in single_page_res:
-                if int(res['category_id']) in [0, 1, 2, 4, 6, 7]:  #categories that need to do ocr
-                    xmin, ymin = int(res['poly'][0]), int(res['poly'][1])
-                    xmax, ymax = int(res['poly'][4]), int(res['poly'][5])
-                    crop_box = [xmin, ymin, xmax, ymax]
-                    cropped_img = Image.new('RGB', pil_img.size, 'white')
-                    cropped_img.paste(pil_img.crop(crop_box), crop_box)
-                    cropped_img = cv2.cvtColor(np.asarray(cropped_img), cv2.COLOR_RGB2BGR)
-                    ocr_res = ocr_model.ocr(cropped_img, mfd_res=single_page_mfdetrec_res)[0]
-                    if ocr_res:
-                        for box_ocr_res in ocr_res:
-                            p1, p2, p3, p4 = box_ocr_res[0]
-                            text, score = box_ocr_res[1]
-                            doc_layout_result[idx]['layout_dets'].append({
-                                'category_id': 15,
-                                'poly': p1 + p2 + p3 + p4,
-                                'score': round(score, 2),
-                                'text': text,
-                            })
-                elif int(res['category_id']) == 5: # do table recognition
-                    xmin, ymin = int(res['poly'][0]), int(res['poly'][1])
-                    xmax, ymax = int(res['poly'][4]), int(res['poly'][5])
-                    crop_box = [xmin, ymin, xmax, ymax]
-                    cropped_img = pil_img.convert("RGB").crop(crop_box)
-                    start = time.time()
-                    with torch.no_grad():
-                        output = tr_model(cropped_img)
-                    end = time.time()
-                    if (end-start) > model_configs['model_args']['table_max_time']:
-                        res["timeout"] = True
-                    res["latex"] = output[0]
-
-
+        
         output_dir = args.output
         os.makedirs(output_dir, exist_ok=True)
         basename = os.path.basename(single_pdf)[0:-4]
         with open(os.path.join(output_dir, f'{basename}.json'), 'w') as f:
             json.dump(doc_layout_result, f)
         
-        if args.vis:
-            color_palette = [
-                (255,64,255),(255,255,0),(0,255,255),(255,215,135),(215,0,95),(100,0,48),(0,175,0),(95,0,95),(175,95,0),(95,95,0),
-                (95,95,255),(95,175,135),(215,95,0),(0,0,255),(0,255,0),(255,0,0),(0,95,215),(0,0,0),(0,0,0),(0,0,0)
-            ]
-            id2names = ["title", "plain_text", "abandon", "figure", "figure_caption", "table", "table_caption", "table_footnote", 
-                        "isolate_formula", "formula_caption", " ", " ", " ", "inline_formula", "isolated_formula", "ocr_text"]
-            vis_pdf_result = []
-            for idx, image in enumerate(img_list):
-                single_page_res = doc_layout_result[idx]['layout_dets']
-                vis_img = Image.new('RGB', Image.fromarray(image).size, 'white') if args.render else Image.fromarray(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-                draw = ImageDraw.Draw(vis_img)
-                for res in single_page_res:
-                    label = int(res['category_id'])
-                    if label > 15:     # categories that do not need visualize
-                        continue
-                    label_name = id2names[label]
-                    x_min, y_min = int(res['poly'][0]), int(res['poly'][1])
-                    x_max, y_max = int(res['poly'][4]), int(res['poly'][5])
-                    if args.render and label in [13, 14, 15]:
-                        try:
-                            if label in [13, 14]:  # render formula
-                                window_img = tex2pil(res['latex'])[0]
-                            else:
-                                if True:           # render chinese
-                                    window_img = zhtext2pil(res['text'])
-                                else:              # render english
-                                    window_img = tex2pil([res['text']], tex_type="text")[0]
-                            ratio = min((x_max - x_min) / window_img.width, (y_max - y_min) / window_img.height) - 0.05
-                            window_img = window_img.resize((int(window_img.width * ratio), int(window_img.height * ratio)))
-                            vis_img.paste(window_img, (int(x_min + (x_max-x_min-window_img.width) / 2), int(y_min + (y_max-y_min-window_img.height) / 2)))
-                        except Exception as e:
-                            print(f"got exception on {text}, error info: {e}")
-                    draw.rectangle([x_min, y_min, x_max, y_max], fill=None, outline=color_palette[label], width=1)
-                    fontText = ImageFont.truetype("assets/fonts/simhei.ttf", 15, encoding="utf-8")
-                    draw.text((x_min, y_min), label_name, color_palette[label], font=fontText)
-                
-                width, height = vis_img.size
-                width, height = int(0.75*width), int(0.75*height)
-                vis_img = vis_img.resize((width, height))
-                vis_pdf_result.append(vis_img)
-            
-            first_page = vis_pdf_result.pop(0)
-            first_page.save(os.path.join(output_dir, f'{basename}.pdf'), 'PDF', resolution=100, save_all=True, append_images=vis_pdf_result)
-            try:
-                shutil.rmtree('./temp')
-            except:
-                pass
             
     now = datetime.datetime.now(tz)
     end = time.time()
